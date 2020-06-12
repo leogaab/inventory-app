b28c1a6aa00427b02c90f9c8b92bdc977de3072b
Leo Gaab <leonelgaab@gmail.com>
2020-06-11 21:29:26 -0300

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductRowComponent } from './components/product-row/product-row.component';
+import { ProductImageComponent } from './components/product-image/product-image.component';
+import { ProductDepartmentComponent } from './components/product-department/product-department.component';
+import { PriceDisplayComponent } from './components/price-display/price-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent
   ],
   imports: [
     BrowserModule,
