a092ae3eb0809136c36bb22810f3b8fb2dbffc68
Leo Gaab <leonelgaab@gmail.com>
2020-07-21 10:04:10 -0300

    updated branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './components/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  // products: Product[];
+  products: Array<Product>;
+
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jacket & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A nice black hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accesories', 'Hats'],
+        29.99)
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked ', product);
+  }
+
 }
